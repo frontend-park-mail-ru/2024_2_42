0d149d0666962e61331f17d91251f351a27ade5c
Ivan Ivashkin <f3shok@gmail.com>
2024-09-29 02:13:35 +0300

    Удалил лишнюю папку modules и исправил константы

--- source/module/validation.js
+++ source/module/validation.js
@@ -1,24 +1,24 @@
-const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-const passwordRegex = /^(?=.*?[A-Z])(?=.*?[0-9]).{8,24}$/;
-const nicknamePattern = /^[0-9a-zA-Z_]{3,20}$/;
+const EMAILPATTERN = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const PASSWORDREGEX = /^(?=.*?[A-Z])(?=.*?[0-9]).{8,24}$/;
+const NICKNAMEPATTERN = /^[0-9a-zA-Z_]{3,20}$/;
 
 
 export const validateInput = (input, rules) => {
     // Валидация email
     if (rules.isEmail) {
-        if (!emailPattern.test(input)) {
+        if (!EMAILPATTERN.test(input)) {
             return { isValid: false, error: 'Неверный адрес электронной почты.' };
         }
     }
 
     // Валидация пароля (8-24 символа, содержит хотя бы одну цифру и одну заглавную букву)
-    if (rules.isPassword && !passwordRegex.test(input)) {
+    if (rules.isPassword && !PASSWORDREGEX.test(input)) {
         return { isValid: false, error: 'Пароль должен содержать от 8 до 24 символов, включать хотя бы одну заглавную букву и одну цифру.' };
     }
 
     // Валидация никнейма (3-20 символов, только цифры, буквы и символ "_")
     if (rules.isNickname) {
-        if (!nicknamePattern.test(input)) {
+        if (!NICKNAMEPATTERN.test(input)) {
             return { isValid: false, error: 'Никнейм должен содержать от 3 до 20 символов и включать только буквы, цифры и символ "_".' };
         }
     }
