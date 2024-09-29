48692b349a99b397de929049e5065979be434080
Fedoseev Vladimir <oruchim@yandex.ru>
2024-09-29 15:39:45 +0300

    поправил ошибки в модуле валидации

--- source/modules/validation.js
+++ source/modules/validation.js
@@ -1,34 +1,43 @@
-/**
- * Validates various types of data.
- * 
- * @param {string} input Data to be validated.
- * @param {object} rules Set of validation rules.
- * @returns {{isValid: boolean, error: string}} Object containing the validation result.
- */
+const EMAIL_PATTERN = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const PASSWORD_PATTERN = /^(?=.*?[A-Z])(?=.*?[0-9]).{8,24}$/;
+const NICKNAME_PATTERN = /^[0-9a-zA-Z_]{3,20}$/;
+
+
 export const validateInput = (input, rules) => {
     // Email validation
     if (rules.isEmail) {
         if (!EMAIL_PATTERN.test(input)) {
-            return { isValid: false, error: 'Invalid email address.' };
+            return { isValid: false, error: 'Неверный формат email. Убедитесь, что введен правильный адрес электронной почты.' };
         }
     }
 
     // Password validation (8-24 characters, must contain at least one uppercase letter and one digit)
-    if (rules.isPassword && !PASSWORD_PATTERN.test(input)) {
-        return { isValid: false, error: 'Password must contain 8 to 24 characters, include at least one uppercase letter and one digit.' };
+    if (rules.isPassword) {
+        if (input.length < 8 || input.length > 24) {
+            return { isValid: false, error: 'Пароль должен содержать от 8 до 24 символов.' };
+        }
+        if (!/[A-Z]/.test(input)) {
+            return { isValid: false, error: 'Пароль должен содержать хотя бы одну заглавную букву.' };
+        }
+        if (!/\d/.test(input)) {
+            return { isValid: false, error: 'Пароль должен содержать хотя бы одну цифру.' };
+        }
     }
 
     // Nickname validation (3-20 characters, only digits, letters, and underscore)
     if (rules.isNickname) {
-        if (!NICKNAME_PATTERN.test(input)) {
-            return { isValid: false, error: 'Nickname must contain 3 to 20 characters and can only consist of digits, letters, and underscores.' };
+        if (input.length < 3 || input.length > 20) {
+            return { isValid: false, error: 'Никнейм должен содержать от 3 до 20 символов.' };
+        }
+        if (!/^[A-Za-z0-9_]+$/.test(input)) {
+            return { isValid: false, error: 'Никнейм может состоять только из букв, цифр и подчеркиваний.' };
         }
     }
 
     // String comparison check (e.g., for password confirmation)
     if (rules.compareWith !== undefined) {
         if (input !== rules.compareWith) {
-            return { isValid: false, error: 'Strings do not match.' };
+            return { isValid: false, error: 'Введенные строки не совпадают. Убедитесь, что они идентичны.' };
         }
     }
 
