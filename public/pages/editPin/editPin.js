aa0f3a5dc5e5d7ba2f3ddec4bf6d9a09b6fdd58d
Alexander Novak <shura.novak.03@mail.ru>
2024-11-06 00:03:13 +0300

    added image input field

--- public/pages/editPin/editPin.js
+++ public/pages/editPin/editPin.js
@@ -6,66 +6,92 @@ import { InputComponent } from '../../components/input/input.js';
  * Component that is used to create and edit pins.
  */
 export default class EditPinComponent extends BaseComponent {
-	#pin;
-	#editMode;
+  #pin;
+  #editMode;
 
-	/**
-	 * Creates a EditPinComponent.
-	 * @param {HTMLElement} parent - HTML element of the parent container.
-	 * @param {Object} pin - object of PinComponent class.
-	 */
-	constructor(parent, pin, editMode) {
-		super(parent);
-		this.#pin = pin;
-		this.#editMode = editMode;
-	}
+  /**
+   * Creates a EditPinComponent.
+   * @param {HTMLElement} parent - HTML element of the parent container.
+   * @param {Object} pin - object of PinComponent class.
+   */
+  constructor(parent, pin, editMode) {
+    super(parent);
+    this.#pin = pin;
+    this.#editMode = editMode;
+    this.handleImageClick = this.handleImageClick.bind(this);
+    this.handleImageUpload = this.handleImageUpload.bind(this);
+  }
 
-	/**
-	 * Renderes a template of the edit pin page.
-	 */
-	renderTemplate() {
-		const template = Handlebars.templates['editPin.hbs'];
-		
-		const titleInput = new InputComponent(this.Parent, {
-			inputPlaceholder: 'Добавьте название *',
-			typeOfInput: 'text',
-		});
-		const descriptionInput = new InputComponent(this.Parent, {
-			inputPlaceholder: 'Добавьте описание',
-			typeOfInput: 'text',
-		});
+  /**
+   * Renderes a template of the edit pin page.
+   */
+  renderTemplate() {
+    const template = Handlebars.templates['editPin.hbs'];
 
-		if (this.#editMode) {
-			titleInput.setValue(this.#pin.Title);
-			descriptionInput.setValue(this.#pin.Description);
-		}
+    const titleInput = new InputComponent(this.Parent, {
+      inputPlaceholder: 'Добавьте название *',
+      typeOfInput: 'text',
+    });
+    const descriptionInput = new InputComponent(this.Parent, {
+      inputPlaceholder: 'Добавьте описание',
+      typeOfInput: 'text',
+    });
 
-		const publishButton = new Button(this.Parent, {
-			label: 'Опубликовать',
-			type: 'submit',
-			disabled: false,
-			hover: false,
-			active: false
-		});
+    if (this.#editMode) {
+      titleInput.setValue(this.#pin.Title);
+      descriptionInput.setValue(this.#pin.Description);
+    }
 
-		let renderedTemplate;
-		if (this.#editMode) {
-			renderedTemplate = template({
-				TitleInput: titleInput.renderTemplate(),
-				DescriptionInput: descriptionInput.renderTemplate(),
-				PublishButton: publishButton.renderTemplate(),
-				InEditMode: this.#editMode,
-				PinBoard: this.#pin.PinBoard,
-			});
-		}
-		else {
-			renderedTemplate = template({
-				TitleInput: titleInput.renderTemplate(),
-				DescriptionInput: descriptionInput.renderTemplate(),
-				PublishButton: publishButton.renderTemplate(),
-			});
-		}
+    const publishButton = new Button(this.Parent, {
+      label: 'Опубликовать',
+      type: 'submit',
+      disabled: false,
+      hover: false,
+      active: false,
+    });
 
-		this.Parent.insertAdjacentHTML('beforeend', renderedTemplate);
-	}
+    let renderedTemplate;
+    if (this.#editMode) {
+      renderedTemplate = template({
+        TitleInput: titleInput.renderTemplate(),
+        DescriptionInput: descriptionInput.renderTemplate(),
+        PublishButton: publishButton.renderTemplate(),
+        InEditMode: this.#editMode,
+        PinBoard: this.#pin.PinBoard,
+      });
+    } else {
+      renderedTemplate = template({
+        TitleInput: titleInput.renderTemplate(),
+        DescriptionInput: descriptionInput.renderTemplate(),
+        PublishButton: publishButton.renderTemplate(),
+      });
+    }
+
+    this.Parent.insertAdjacentHTML('beforeend', renderedTemplate);
+    const imageContainer = this.Parent.querySelector(
+      '.editpin__image-container'
+    );
+    const imageInput = this.Parent.querySelector('#editpin__image-input');
+    imageContainer.addEventListener('click', this.handleImageClick);
+    imageInput.addEventListener('change', this.handleImageUpload);
+  }
+
+  handleImageClick(event) {
+    event.preventDefault();
+    const imageInput = this.Parent.querySelector('#editpin__image-input');
+    imageInput.click();
+  }
+
+  async handleImageUpload(event) {
+    event.preventDefault();
+    const imageInput = event.target;
+
+    if (imageInput.files && imageInput.files.length > 0) {
+      const file = imageInput.files[0];
+      const formData = new FormData();
+      formData.append(this.filename, file);
+
+      const responce = await fetch;
+    }
+  }
 }
