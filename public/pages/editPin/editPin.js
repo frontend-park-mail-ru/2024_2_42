8ea79573d504b10e15a6199013bbb0522c63108e
Ivan Ivashkin <f3shok@gmail.com>
2024-11-09 13:11:01 +0300

    8

--- public/pages/editPin/editPin.js
+++ public/pages/editPin/editPin.js
@@ -123,7 +123,6 @@ export default class EditPinComponent extends BaseComponent {
       title: TitleValue,
       media_url: ImageUrl,
       related_links: ImageUrl,
-      board_id: 1,
     });
 
     console.log(requestBody);
