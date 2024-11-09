09d97f549e3367b6a794bdc902e4b54996520871
Ivan Ivashkin <f3shok@gmail.com>
2024-11-09 12:25:52 +0300

    12

--- public/pages/editPin/editPin.js
+++ public/pages/editPin/editPin.js
@@ -131,7 +131,7 @@ export default class EditPinComponent extends BaseComponent {
     //   body: requestBody,
     // });
 
-    await fetch('http://37.139.41.77:8080/create-pin', {
+    await fetch('http://37.139.41.77:8079/create-pin', {
       method: 'POST',
       mode: 'cors',
       credentials: 'include',
