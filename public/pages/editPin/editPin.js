9f1e5019387aca2ac9d487f30df8b417e917aae0
Ivan Ivashkin <f3shok@gmail.com>
2024-11-09 11:57:26 +0300

    Добавил роуты

--- public/pages/editPin/editPin.js
+++ public/pages/editPin/editPin.js
@@ -87,7 +87,7 @@ export default class EditPinComponent extends BaseComponent {
       const formData = new FormData();
       formData.append('file', file);
 
-      const response = await fetch('http://localhost:8080/image/upload', {
+      const response = await fetch('http://37.139.41.77:8079/image/upload', {
         method: 'POST',
         body: formData,
       });
