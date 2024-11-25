10dbfdc03644b24e4d7ef06f54d6e98b7820c381
Ivan Ivashkin <f3shok@gmail.com>
2024-11-25 16:28:40 +0300

    Исправление конфликтов

--- public/components/input/input.js
+++ public/components/input/input.js
@@ -7,57 +7,56 @@ import { BaseComponent } from '../base/base.js';
  * @class
  */
 export class InputComponent extends BaseComponent {
-    /**
-     * The function that will handle the input change event.
-     * @type {Function}
-     */
-    #changeHandler = () => {};
+	/**
+	 * The function that will handle the input change event.
+	 * @type {Function}
+	 */
+	#changeHandler = () => { };
 
-    /**
-     * Creates an instance of InputComponent.
-     * @constructor
-     * @param {HTMLElement} parent - The parent element where the input will be rendered.
-     * @param {Object} [state] - The initial state of the input component. (optional)
-     * @param {Function} [changeHandler] - The function that will handle the input change event. (optional)
-     */
-    constructor(parent, state = {}, changeHandler = () => {}) {
-        super(parent, state);
-        this.#changeHandler = changeHandler;
-    }
+	/**
+	 * Creates an instance of InputComponent.
+	 * @constructor
+	 * @param {HTMLElement} parent - The parent element where the input will be rendered.
+	 * @param {Object} [state] - The initial state of the input component. (optional)
+	 * @param {Function} [changeHandler] - The function that will handle the input change event. (optional)
+	 */
+	constructor(parent, state = {}, changeHandler = () => { }) {
+		super(parent, state);
+		this.#changeHandler = changeHandler;
+	}
 
-    /**
-     * Renders the input component.
-     * @returns {string} - The rendered HTML template of the input.
-     */
-    renderTemplate() {
-        const template = Handlebars.templates['input.hbs'];
-        const renderedTemplate = template(this.State);
+	/**
+	 * Renders the input component.
+	 * @returns {string} - The rendered HTML template of the input.
+	 */
+	renderTemplate() {
+		const template = Handlebars.templates['input.hbs'];
+		const renderedTemplate = template(this.State);
+		const parent = this.Parent;
+		if (parent) {
+			parent.innerHTML += renderedTemplate;
 
-        const parent = this.Parent;
-        if (parent) {
-            parent.innerHTML += renderedTemplate;
-        }
+		}
+		return renderedTemplate;
+	}
 
-        return renderedTemplate;
-    }
+	/**
+	 * Handles the input change event.
+	 * @param {Event} event - The input change event object.
+	 */
+	handleInputChange(event) {
+		if (typeof this.#changeHandler === 'function') {
+			this.#changeHandler(event.target.value);
+		}
+	}
 
-    /**
-     * Handles the input change event.
-     * @param {Event} event - The input change event object.
-     */
-    handleInputChange(event) {
-        if (typeof this.#changeHandler === 'function') {
-            this.#changeHandler(event.target.value);
-        }
-    }
-
-    /**
-     * Sets the change event handler for the input.
-     * @param {Function} changeHandler - The function to handle the input change event.
-     */
-    setChangeHandler(changeHandler) {
-        this.#changeHandler = changeHandler;
-    }
+	/**
+	 * Sets the change event handler for the input.
+	 * @param {Function} changeHandler - The function to handle the input change event.
+	 */
+	setChangeHandler(changeHandler) {
+		this.#changeHandler = changeHandler;
+	}
 
 	/**
 	 * Gets the current change event handler for the input.
